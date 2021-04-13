788ab3bf1201151887d260dab3d1f1110fd9cadc
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2021-04-13 09:49:30 -0400

    start

---
+++ README.md
@@ -0,0 +1,3 @@
+Cypress hover example
+
+This example shows how to trigger the hover state using `mouseover` and true CSS hover state via [cypress-real-events](https://github.com/dmtrKovalenko/cypress-real-events) plugin
--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,15 +1,5 @@
 /// <reference types="cypress" />
 
-describe('hover', () => {
-  it('works with mouseover', () => {
-    cy.visit('test-hover.html')
-    // at first elements are invisible
-    cy.get('#left11').should('not.be.visible')
-    cy.get('#menu1').trigger('mouseover')
-    // and the elements should be visible
-    cy.get('#left11').should('be.visible')
-    // hide
-    cy.get('#menu1').trigger('mouseout')
-    cy.get('#left11').should('not.be.visible')
-  })
+it('hovers', () => {
+  cy.visit('test-hover.html')
 })
