c7680b711ed0fb01d20161eaa73bd961490c7da2
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2021-04-13 10:09:05 -0400

    write spec

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,5 +1,21 @@
 /// <reference types="cypress" />
-
+import "cypress-real-events/support";
 it('hovers', () => {
   cy.visit('test-hover.html')
+  cy.get('#left11').should('not.be.visible')
+  cy.get('#menu1').trigger('mouseover')
+  cy.get('#left11').should('be.visible')
+    .wait(1000)
+  cy.get('#menu1').trigger('mouseout')
+  cy.get('#left11').should('not.be.visible')
+})
+
+it.only(':hovers', () => {
+  cy.visit('test-hover.html')
+  cy.get('#left21').should('not.be.visible')
+  cy.get('#menu2').realHover()
+  cy.get('#left21').should('be.visible')
+    .wait(1000)
+  cy.get('h1').realHover()
+  cy.get('#left21').should('not.be.visible')
 })
