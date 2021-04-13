967242bb2f26e7bbe938cfe9bbf51178d70d7996
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2021-04-12 21:39:12 -0400

    test mouseover

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,3 +1,15 @@
 /// <reference types="cypress" />
 
-it('works', () => {})
+describe('hover', () => {
+  it('works with mouseover', () => {
+    cy.visit('test-hover.html')
+    // at first elements are invisible
+    cy.get('#left11').should('not.be.visible')
+    cy.get('#menu1').trigger('mouseover')
+    // and the elements should be visible
+    cy.get('#left11').should('be.visible')
+    // hide
+    cy.get('#menu1').trigger('mouseout')
+    cy.get('#left11').should('not.be.visible')
+  })
+})
